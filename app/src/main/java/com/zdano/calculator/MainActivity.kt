84e9f89348e1cd0c8d9be1cbdc8602cc11724c6d
INFJakZda <zdanowski78@gmail.com>
2018-04-03 00:21:30 +0200

    Add number to count and make stack

--- app/src/main/java/com/zdano/calculator/MainActivity.kt
+++ app/src/main/java/com/zdano/calculator/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.zdano.calculator
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val numberCache: MutableList<String> = arrayListOf()
+    val stackNumbers: MutableList<String> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,5 +35,38 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun updateDisplay(mainDisplayString: String){
+
+        //val fullCalculationString = makeString(operationList, " ")
+        //var fullCalculationTextView = findViewById(R.id.fullCalculationText) as TextView
+        //fullCalculationTextView.text = fullCalculationString
+
+        val mainTextView = findViewById(R.id.formula) as TextView
+        mainTextView.text = mainDisplayString
+    }
+
+    fun makeString(list: List<String>,joiner: String = "") : String {
+
+        if (list.isEmpty()) return ""
+        return list.reduce { r, s -> r + joiner + s }
+    }
+
+    fun numberClick(view: View) {
+        val button = view as Button
+        val numberString = button.text;
+
+        numberCache.add(numberString.toString())
+        val text = makeString(numberCache);
+        updateDisplay(text)
+    }
+
+    fun enterClick(view: View) {
+        //val mainTextView = findViewById(R.id.formula) as TextView
+        stackNumbers.add(formula.text.toString())
+        result.text = result.text.toString().plus("\n").plus(stackNumbers.last())
+        //mainTextView.text = ""
+        //numberCache.clear()
     }
 }
